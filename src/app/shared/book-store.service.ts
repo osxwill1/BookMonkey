1934f8d80e7baa18002fb027288880ee86389e01
Florian Will <osx.will1@gmail.com>
2019-10-08 15:49:02 +0200

    RxJS (BookRaw & BookFactory)

--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Book } from './book';
+import { BookRaw } from './book-raw';
+import { BookFactory } from './book-factory';
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +16,19 @@ export class BookStoreService {
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<Book[]> {
-    return this.http.get<any[]>(`${this.api}/books`);
+    return this.http.get<BookRaw[]>(`${this.api}/books`)
+    .pipe(
+      map(booksRaw =>
+        booksRaw.map(b => BookFactory.fromRaw(b))
+      )
+    );
   }
 
   getSingle(isbn: string): Observable<Book> {
-    return this.http.get<any>(
+    return this.http.get<BookRaw>(
       `${this.api}/book/${isbn}`
+    ).pipe(
+      map(b => BookFactory.fromRaw(b))
     );
   }
 
